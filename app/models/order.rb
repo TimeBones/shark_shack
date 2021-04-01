ff070737787184fde1254ae110e0875dbd17129c
timebones <bendpenner@gmail.com>
2021-04-01 09:37:22 -0500

    Fixed model validation for numbers

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_products
   validates :date, :subtotal, :user_id, :status, presence: true
-  validates :subtotal, is_numeric: true
+  validates :subtotal, numericality: { only_integer: true }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ApplicationRecord
   has_many :product_categories
   has_many :categories, through: :product_categories
   validates :name, :price, :desc, :status, :weight, presence: true
-  validates :price, :status, :weight, is_numeric: true
+  validates :price, :status, :weight, numericality: { only_integer: true }
   has_one_attached :image
 end
