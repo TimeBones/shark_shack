4e68399814a2964371c4190ca43f40d8e05fc1c1
timebones <bendpenner@gmail.com>
2021-04-02 11:39:55 -0500

    added devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :orders
+  validates :username, :password, :powerlevel, :active, presence: true
+  validates :username, uniqueness: true
+  validates :powerlevel, :active, numericality: { only_integer: true }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :cart, only: %i[create destroy index]
   resources :products, only: %i[show index]
   resources :categories, only: %i[show index]
 
-  get "/account", to: "users#show"
   get "/about", to: "pages#about"
+  get "/logout", to: "users#logout"
+  get "/account", to: "users#show"
+  get "/login", to: "users#login", as: "user_login"
+  get "/newaccount", to: "users#newaccount"
 
   scope "/checkout" do
     post "create", to: "checkout#create", as: "checkout_create"
