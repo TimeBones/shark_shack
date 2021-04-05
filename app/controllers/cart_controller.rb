1c68174a15c567f51a63165ddd50de0d1d134c12
timebones <bendpenner@gmail.com>
2021-04-04 19:25:03 -0500

    Cart adding product and quantity

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,12 +3,15 @@ class CartController < ApplicationController
     id = params[:id].to_i
     qty = params[:qty].to_i
 
-    session[:shopping_cart] << { id => qty }
-    # product = Product.find(id)
-    # flash[:notice] = "Added #{product.name} to the cart."
-    # redirect_to root_path
+    session[:shopping_cart][id] = if session[:shopping_cart].key?(id.to_s)
+                                    session[:shopping_cart].fetch(id.to_s) + qty
+                                  else
+                                    qty
+                                  end
+    product = Product.find(id)
+    flash[:notice] = "Added #{product.name} to the cart."
     @cart = session[:shopping_cart]
-
+    redirect_to root_path
   end
 
   def destroy
